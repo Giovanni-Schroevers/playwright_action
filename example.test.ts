3f9da260e8d9de4c83d62eb3898a67a127fe7d2e
Giovanni-Schroevers <giovie2000@hotmail.com>
2021-10-15 22:20:37 +0200

    Reduce timeout

--- example.test.ts
+++ example.test.ts
@@ -2,8 +2,8 @@ import { test, errors } from '@playwright/test';
 
 test('basic test', async ({ page }) => {
   try {
-    await page.goto('https://12gobiking.nl/racefietsen', {timeout: 100000});
-    await page.goto('https://12gobiking.nl/racefietsen', {timeout: 100000});
+    await page.goto('https://12gobiking.nl/racefietsen');
+    await page.goto('https://12gobiking.nl/racefietsen', {timeout: 10000});
   } catch (error) {
     if (error instanceof errors.TimeoutError)
       console.log("Timeout!")
