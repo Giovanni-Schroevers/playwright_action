c69d5715ad91fec3bb0a5635b186a489c802c0ca
Giovanni-Schroevers <giovie2000@hotmail.com>
2021-10-15 22:13:31 +0200

    Fix import

--- example.test.ts
+++ example.test.ts
@@ -1,13 +1,11 @@
-import { test } from '@playwright/test';
-const playwright = require('playwright');
+import { test, errors } from '@playwright/test';
 
 test('basic test', async ({ page }) => {
   try {
     await page.goto('https://12gobiking.nl/racefietsen', {timeout: 100000});
     await page.goto('https://12gobiking.nl/racefietsen', {timeout: 100000});
   } catch (error) {
-    if (error instanceof playwright.errors.TimeoutError)
+    if (error instanceof errors.TimeoutError)
       console.log("Timeout!")
   }
-  await browser.close();
 });
